9b3b5173bbe339df2f4934d1035f183081c49da9
alexa <alexandrohipolito@gmail.com>
2023-07-19 14:21:50 -0300

    Initial commit

---
+++ app/src/main/java/tutoriaisandroid/dei/estg/ipleria/android7/MainActivity.kt
@@ -0,0 +1,46 @@
+package tutoriaisandroid.dei.estg.ipleria.android7
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tutoriaisandroid.dei.estg.ipleria.android7.ui.theme.Android7Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Android7Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android7Theme {
+        Greeting("Android")
+    }
+}
